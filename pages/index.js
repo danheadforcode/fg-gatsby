a17d0251aacc079f580e7115355f8a5c228888be
Nick Lewis <nick@headforcode.com>
2017-06-22 17:48:05 +0100

    Changes made to the index page to import both the jumbotron and the intro block

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from 'react-helmet'
 import { config } from 'config'
+import JumboTron from 'components/JumboTron'
+import IntroBlock from 'components/IntroBlock'
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
+        <JumboTron />
+        <IntroBlock />
+
         <h1>
           Hi Daniel
         </h1>
@@ -15,6 +20,7 @@ export default class Index extends React.Component {
         <p>Now go build something great.</p>
         <Link to={prefixLink('/page-2/')}>Go to page 2</Link>
       </div>
+      
     )
   }
 }
