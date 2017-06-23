74718985453b5b47c1b8166f547b01a1b0026e81
Nick Lewis <nick@headforcode.com>
2017-06-23 18:50:13 +0100

    Fixed issue with classNames in this component, it now renders correctly

--- components/IntroBlock.js
+++ components/IntroBlock.js
@@ -4,27 +4,27 @@ class IntroBlock extends React.Component {
   render() {
 
     return (
-      <div class="db dt-ns mw9 center">
-    <div class="fl-m fl-l w-33-m w-33-l">
-        <div class="pa4 pa4-m">
-            <h3 class="f3">Whats in the Name?</h3>
-            <p class="mid-gray f4 lh-copy"></p><p>Farnborough was mentioned in the domesday book of 1086, a Saxon settlement. The name Farnborough is derived from "Ferneberga" believed to mean "Fern Hill". There are many links to this ancient name across the town. In fact our office is in the old Town Hall, Ferneberga House.</p>
+      <div className="db dt-ns mw9 center">
+    <div className="fl-m fl-l w-33-m w-33-l">
+        <div className="pa4 pa4-m">
+            <h3 className="f3">Whats in the Name?</h3>
+            <p className="mid-gray f4 lh-copy"></p><p>Farnborough was mentioned in the domesday book of 1086, a Saxon settlement. The name Farnborough is derived from "Ferneberga" believed to mean "Fern Hill". There are many links to this ancient name across the town. In fact our office is in the old Town Hall, Ferneberga House.</p>
 <p><a data-udi="umb://document/01f3da0532eb4f4bba19be827f2df3e1" href="/whats-in-the-name/" title="Whats in the Name?">Read more?</a></p><p></p>
         </div>
     </div>
-    <div class="fl-m fl-l w-33-m w-33-l">
-        <div class="pa4 pa4-m">
-            <h3 class="f3">Aircraft and Aviation</h3>
-            <p class="mid-gray f4 lh-copy measure-narrow"></p><p><span>Over the years Farnborough has played a major role in the British aviation industry and is linked with three particular milestones in the history of the industry that has ultimately led to faster travel across the globe.&nbsp;</span><span>&nbsp;</span></p>
+    <div className="fl-m fl-l w-33-m w-33-l">
+        <div className="pa4 pa4-m">
+            <h3 className="f3">Aircraft and Aviation</h3>
+            <p className="mid-gray f4 lh-copy measure-narrow"></p><p><span>Over the years Farnborough has played a major role in the British aviation industry and is linked with three particular milestones in the history of the industry that has ultimately led to faster travel across the globe.&nbsp;</span><span>&nbsp;</span></p>
 <p>&nbsp;</p>
 <p><a data-udi="umb://document/46b18f7dda744e879849c44db072137f" href="/aircraft-and-aviation/" title="Aircraft and Aviation">Read more?</a></p>
 <p>&nbsp;</p><p></p>
         </div>
     </div>
-    <div class="fl-m fl-l w-33-m w-33-l">
-        <div class="pa4 pa4-m">
-            <h3 class="f3">The Future...</h3>
-            <p class="mid-gray f3 lh-copy measure-narrow"></p><p><span>Farnborough is a town that has always been in a state of transition mainly due to the industry here and it's location with excellent connections to London, Guildford, Basingstoke and Southampton. So it is no surprise that it </span><span>continues to be attractive to investors in business and property.&nbsp;</span><span>&nbsp;</span></p>
+    <div className="fl-m fl-l w-33-m w-33-l">
+        <div className="pa4 pa4-m">
+            <h3 className="f3">The Future...</h3>
+            <p className="mid-gray f3 lh-copy measure-narrow"></p><p><span>Farnborough is a town that has always been in a state of transition mainly due to the industry here and it's location with excellent connections to London, Guildford, Basingstoke and Southampton. So it is no surprise that it </span><span>continues to be attractive to investors in business and property.&nbsp;</span><span>&nbsp;</span></p>
 <p><a data-udi="umb://document/181b80d665f14a45afbe0d5c786c8e5a" href="/the-future/" title="The Future...">Read more?</a></p><p></p>
         </div>
     </div>
