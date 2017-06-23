e2c61122e822858ea98a91ad67763582c513dacd
Nick Lewis <nick@headforcode.com>
2017-06-23 18:49:22 +0100

    Finished off the eventslist and we can expand on this further over time

--- components/EventsList.js
+++ components/EventsList.js
@@ -1,9 +1,25 @@
 import React from 'react'
+
 // DAN: This component will work once you import it in the index.js file
 class EventsList extends React.Component {
   render() {
     return (
-        <div>No worries Dan, I took out what was here, we shall come back to this later to change it</div>
+        
+        <div className="db dt-ns mw9 center">
+            <div className="pa4 pa4-m">
+                <h3 className="f3">Featured Events</h3>
+            </div>
+		    <div className="fl-m fl-l w-25-m w-25-l">
+                <div className="pa4 pa4-m">
+                    <a href="/events/carbon-trust/"><img src={require('./carbon-trust.jpg')} alt="carbon-trust.jpg" /></a>
+                </div>
+            </div>
+	        <div className="fl-m fl-l w-25-m w-25-l">
+                <div className="pa4 pa4-m">
+                    <a href="/events/"><img src={require('./number3.png')} alt="See more events or add your own" /></a>
+                </div>
+            </div>
+        </div>
     )}
 }
 
