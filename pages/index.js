c0f0cbccbaa1f1c0aa2da3f5e09a1df7066300b5
Nick Lewis <Nick.Lewis@pcl.co.uk>
2017-06-23 08:35:00 +0100

    Added instruction comments for Dan

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,33 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from 'react-helmet'
 import { config } from 'config'
+
+// Import components needed for this page
+// - to learn more about what the following components do, just take a
+// - look at the their own files under the components folder in the project
 import JumboTron from 'components/JumboTron'
 import IntroBlock from 'components/IntroBlock'
 
+// The following line is commented out because the component it imports
+// has not been created yet
+
+// import IntroBlock from 'components/EventsList'
+
+// The Home page component
 export default class Index extends React.Component {
   render() {
     return (
       <div>
+
+        { /* add our pages components */ }
         <JumboTron />
         <IntroBlock />
 
+        { /* We shall insert more components here for: */ }
+
+        { /* <EventsList /> */ }
+
+        { /* you can delete the stuff under this comment line */ }
         <h1>
           Hi Daniel
         </h1>
