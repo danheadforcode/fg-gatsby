0389aa97b65124466dca2b03e5c567d4a93112df
Nick Lewis <nick@headforcode.com>
2017-06-29 13:34:01 +0100

    Fixes for eventslist

--- components/EventsList/index.js
+++ components/EventsList/index.js
@@ -1,21 +1,41 @@
 import React from 'react'
+import sortBy from 'lodash/sortBy'
+import get from 'lodash/get'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
 
 // DAN: This component will work once you import it in the index.js file
 class EventsList extends React.Component {
   render() {
-     
+      // Sort pages.      
+    const sortedPages = sortBy(this.props.route.pages, 'data.date')    
+    // Posts are those with md extension that are not 404 pages OR have a date (meaning they're a react component post).
+    const visiblePages = sortedPages.filter(page => (
+      get(page, 'file.ext') === 'md' && get(page, 'data.type' === 'event' ) || get(page, 'data.date')
+    ))
     return (
         
         <div className="db dt-ns mw9 center">
             <div className="pa4 pa4-m">
                 <h3 className="f3">Featured Events</h3>
             </div>
-		    <div className="fl-m fl-l w-25-m w-25-l">
+		
+
+            {visiblePages.map((page) => (
+              <div key={page.path} className="fl-m fl-l w-25-m w-25-l">
                 <div className="pa4 pa4-m">
-                    <a href="/events/carbon-trust/"><img src={require('../../pages/events/carbon-trust/carbon-trust.jpg')} alt="carbon-trust.jpg" /></a>
+                    <Link style={{boxShadow: 'none'}} to={prefixLink(page.path)}>
+                    <img src={ prefixLink(page.path) + get(page, 'data.image', page.path)} alt="carbon-trust.jpg" />
+                </Link>
+             
                 </div>
             </div>
-	        <div className="fl-m fl-l w-25-m w-25-l">
+             
+              
+              
+          ))}
+
+     <div className="fl-m fl-l w-25-m w-25-l">
                 <div className="pa4 pa4-m">
                     <a href="/events/"><img src={require('./number3.png')} alt="See more events or add your own" /></a>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
+
+
 import { rhythm } from 'utils/typography'
 import Helmet from 'react-helmet'
 import { config } from 'config'
-import sortBy from 'lodash/sortBy'
-import get from 'lodash/get'
 import EventsList from 'components/EventsList'
 // Import components needed for this page
 // - to learn more about what the following components do, just take a
@@ -22,12 +20,7 @@ import IntroBlock from 'components/IntroBlock'
 // The Home page component
 class Index extends React.Component {
   render() {
-     // Sort pages.      
-    const sortedPages = sortBy(this.props.route.pages, 'data.date')
-    // Posts are those with md extension that are not 404 pages OR have a date (meaning they're a react component post).
-    const visiblePages = sortedPages.filter(page => (
-      get(page, 'file.ext') === 'md' && get(page, 'data.type' === 'event' ) || get(page, 'data.date')
-    ))
+    
     return (
       <div>
 
@@ -37,35 +30,10 @@ class Index extends React.Component {
 
         { /* DAN: uncomment the line below to activate the code */ }
 
-        <EventsList />
+        <EventsList route={this.props.route} />
         
 
-         <div className="db dt-ns mw9 center">
-            <div className="pa4 pa4-m">
-                <h3 className="f3">Featured Events</h3>
-            </div>
-		
-
-            {visiblePages.map((page) => (
-              <div key={page.path} className="fl-m fl-l w-25-m w-25-l">
-                <div className="pa4 pa4-m">
-                    <Link style={{boxShadow: 'none'}} to={prefixLink(page.path)}>
-                    <img src={ prefixLink(page.path) + get(page, 'data.image', page.path)} alt="carbon-trust.jpg" />
-                </Link>
-             
-                </div>
-            </div>
-             
-              
-              
-          ))}
-
-     <div className="fl-m fl-l w-25-m w-25-l">
-                <div className="pa4 pa4-m">
-                    <a href="/events/"><img src={require('./number3.png')} alt="See more events or add your own" /></a>
-                </div>
-            </div>
-        </div>
+         
         
       </div>
 
